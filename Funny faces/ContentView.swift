edba0444fa7e7450145f813b7cda473ff2ed5524
Gianluca Orpello <g.orpello@gmail.com>
2025-07-02 00:25:07 +0200

    Initial Commit

---
+++ Funny faces/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Funny faces
+//
+//  Created by Gabriella Annunziata on 02/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Funny faces/Funny_facesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Funny_facesApp.swift
+//  Funny faces
+//
+//  Created by Gabriella Annunziata on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Funny_facesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
